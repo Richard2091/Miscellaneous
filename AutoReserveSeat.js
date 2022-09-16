1c02a38e77ce6115ac7478f4143247810a6072e7
Richard <53105011+Richard2091@users.noreply.github.com>
2022-09-17 00:02:09 +0800

    优化代码
    优化 每次请求单独获取token
    删除 超时设置
    新增 最大尝试次数
    优化 请求间隔时间

--- AutoReserveSeat.js
+++ AutoReserveSeat.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        自动预约座位
-// @version     0.7
+// @version     0.8
 // @author      Richard
 // @description 定时自动模拟请求预约座位
 // @grant       none
@@ -22,15 +22,15 @@
     var roomId = '905';
     //座位号(输入完整编号,前面如果有0就要带0)
     var seatNum = '001';
-    //开始运行时间(默认21:30 注意前面不要带0, 如01, 直接写1即可)
-    var startHour = 21, startMinute = 30;
-    //运行时长(超过此时间后停止运行, 不能为0和小数, 预约成功会提前停止, 单位:分钟)
-    var runMinute = 1;
     //预约时间(每行代表一次预约,一行内前者表示开始时间,后者表示结束时间)
     var timeList = [["08:30","12:30"],
                     ["12:30","14:00"],
                     ["14:00","18:00"],
                     ["18:00","21:30"]];
+    //开始运行时间(默认21:30:00 注意前面不要带0, 如01, 直接写1即可)
+    var startHour = 21, startMinute = 30;
+    //最大尝试次数(如果预约不成功且预约次数低于此值时会自动重新预约, 预约所有时间段算一次)
+    var maxTryNum = 10;
     //推送服务密钥(访问此页面获取密钥https://sct.ftqq.com/sendkey 留空则不推送)
     var sendKey = "";
   
@@ -49,11 +49,11 @@
     var timeNow = new Date();
     var hour, minute, second;
     
-    //预约是否全部成功
-    var allSuccess = false;
+    //预约成功次数
+    var successNum = 0;
   
-    //预约座位
-    function reserveSeat(){
+    //发起单次预约
+    function reserveSeat(startTime, endTime){
       //用于接收页面返回值
       var htmlData = null;
       //获取页面数据
@@ -67,34 +67,48 @@
       });
       //提取token
       var token = htmlData.split("token: '")[1].split("'")[0];
-      //模拟多次请求
-      for(var i=0; i<timeList.length; i++){
-        //合成链接
-        var reserveURL = "http://office.chaoxing.com/data/apps/seatengine/submit?roomId="+roomId+"&startTime="+timeList[i][0]+"&endTime="+timeList[i][1]+"&day="+tomorrow+"&captcha=&seatNum="+seatNum+"&token="+token;
-        //接收预约结果
-        var reserveResult = null;
-        //发送请求
-        $.ajax({
-          type: "GET",
-          url: reserveURL,
-          async: false,
-          dataType:'json',
-          success: function(json) {
-            reserveResult = json.msg
+      //定义链接
+      var reserveURL = "http://office.chaoxing.com/data/apps/seatengine/submit?";
+      var reserveParameter = "roomId="+roomId+"&startTime="+startTime+"&endTime="+endTime+"&day="+tomorrow+"&captcha=&seatNum="+seatNum+"&token="+token;
+      //接收预约结果
+      var reserveResult = null;
+      //发送请求
+      $.ajax({
+        type: "GET",
+        url: reserveURL+reserveParameter,
+        async: false,
+        dataType:'json',
+        success: function(json) {
+          if(json.success || json.msg=="该时间段您已有预约！"){
+            reserveResult = "预约成功";
+          }else{
+            reserveResult = json.msg;
           }
-        });
-        //输出返回值
-        console.log(seatNum+" "+timeList[i][0]+"-"+timeList[i][1]+" 预约结果: "+reserveResult);
-        //判断预约结果
-        if(reserveResult != "该时间段您已有预约！"){
-          //预约失败
-          allSuccess = false;
         }
+      });
+      //输出返回值
+      var information = "座位 "+seatNum+" 时间段"+startTime+"-"+endTime+" "+reserveResult;
+      console.log(information);
+      //处理结果
+      if(reserveResult == "预约成功"){
+        //如果填了sendKey则发起推送
+        if(sendKey!=""){
+          $.ajax({
+            type: "GET",
+            url: "https://sctapi.ftqq.com/"+sendKey+".send?title="+information,
+            async: true,
+            success: function(data) {
+              console.log(data);
+            }
+          });
+        }
+        //记录成功次数
+        successNum++;
       }
     }
     
-    //时间更新器
-    function timeUpdater(){
+    //时间函数
+    function timeUtils(){
       //初始化日期数据
       year = today.getFullYear(), month = today.getMonth()+1, date = today.getDate();
       console.log("日期数据已获取, 现在是"+year+"年"+month+"月"+date+"日");
@@ -103,50 +117,37 @@
 
       //每秒更新时间数据
       let updateTime = setInterval(() => {
+        //更新时间数据
         timeNow = new Date();
         hour = timeNow.getHours(), minute = timeNow.getMinutes(), second = timeNow.getSeconds();
+        
         //抢座前5秒输出倒计时
         if(hour==startHour && minute==startMinute-1 && second>=55){
           console.log("正在倒计时, 现在是 "+hour+":"+minute+":"+second);
         }
-        //到点开始运行, 超时停止预约
-        if(hour==startHour && minute>=startMinute && minute<runMinute+startMinute){
-          //如果还未成功,则继续预约
-          if(!allSuccess){
-            //将预约结果重置
-            allSuccess = true;
-            //提示开始预约
-            console.log("现在是 "+hour+":"+minute+":"+second+" 开始预约");
-            //保存过程信息
-            var information = hour+":"+minute+":"+second+" 发起预约。\n";
-            //开始预约
-            reserveSeat();
-            //处理预约结果
-            if(allSuccess){
-              //输出成功提示
-              console.log(tomorrow+" "+seatNum+" 所有时间段预约成功");
-              //保存结果信息
-              information += tomorrow+" "+seatNum+" 所有时间段预约成功";
-              //判断是否推送
-              if(sendKey != ""){
-                //发起推送
-                $.ajax({
-                  type: "GET",
-                  url: "https://sctapi.ftqq.com/"+sendKey+".send?title=座位自动预约成功&desp="+information,
-                  async: false,
-                  success: function(data) {
-                    console.log(data);
-                  }
-                });
-              }
+        
+        //到点开始运行
+        if(hour==startHour && minute==startMinute && second==0){
+          //反复发起预约
+          for(var reserveNum=0; reserveNum<maxTryNum; reserveNum++){
+            //全部成功则中止
+            if(successNum == timeList.length){
+              break;
             }
+            //提示当前次数
+            console.log("发起第 "+(reserveNum+1)+"次预约");
+            //遍历时间表
+            for(var j=0; j<timeList.length; j++){
+              reserveSeat(timeList[j][0], timeList[j][1]);
+            }  
           }
         }
+        
       }, 1000);
     }
     
     //检测时间
-    timeUpdater();
+    timeUtils();
   
 })();
 
