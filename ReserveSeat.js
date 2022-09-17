b4ba0a735506565ef8fa9e5ee68582e9f9148c5d
Richard <53105011+Richard2091@users.noreply.github.com>
2022-09-18 07:22:00 +0800

    修复 单个token用于多次请求的问题

--- ReserveSeat.js
+++ ReserveSeat.js
@@ -6,7 +6,7 @@ var roomId = '905';
 //座位号(输入完整编号,前面如果有0就要带0)
 var seatNum = '001';
 //预约日期(比如今天是9月16日,需要提前预约明天的座位,就填2022-09-17, 注意格式)
-var reserveTime = '2022-09-17';
+var reserveTime = '2022-09-18';
 //预约时间(每行代表一次预约,一行内前者表示开始时间,后者表示结束时间)
 var timeList = [["08:30","12:30"],
                 ["12:30","14:00"],
@@ -16,20 +16,20 @@ var timeList = [["08:30","12:30"],
 
 
 ////////////下面的代码不要乱改////////////
-var htmlData = null;
-//获取页面数据
-$.ajax({
-    type: "GET",
-    url: "http://office.chaoxing.com/front/third/apps/seatengine/select?id="+roomId+"&day="+reserveTime+"&backLevel=2&seatId=661&fidEnc=e8d15c598859417b",
-    async: false,
-    success: function(data) {
-        htmlData = data;
-    }
-});
-//提取token
-var token = htmlData.split("token: '")[1].split("'")[0];
-//模拟多次请求
 for(var i=0; i<timeList.length; i++){
+	var htmlData = null;
+	//获取页面数据
+	$.ajax({
+	    type: "GET",
+	    url: "http://office.chaoxing.com/front/third/apps/seatengine/select?id="+roomId+"&day="+reserveTime+"&backLevel=2&seatId=661&fidEnc=e8d15c598859417b",
+	    async: false,
+	    success: function(data) {
+	        htmlData = data;
+	    }
+	});
+	//提取token
+	var token = htmlData.split("token: '")[1].split("'")[0];
+	//模拟多次请求
     //合成链接
     var reserveURL = "http://office.chaoxing.com/data/apps/seatengine/submit?roomId="+roomId+"&startTime="+timeList[i][0]+"&endTime="+timeList[i][1]+"&day="+reserveTime+"&captcha=&seatNum="+seatNum+"&token="+token;
     //接收预约结果
@@ -38,14 +38,12 @@ for(var i=0; i<timeList.length; i++){
     $.ajax({
         type: "GET",
         url: reserveURL,
-        async: false,
+        async: true,
         dataType:'json',
         success: function(json) {
             reserveResult = json.msg
+		    //输出返回值
+		    console.log(" 预约结果:"+reserveResult);
         }
     });
-    //输出返回值
-    console.log(timeList[i][0]+"-"+timeList[i][1]+" 预约结果:"+reserveResult);
 }
-
-
