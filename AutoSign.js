831b3ffb6eb296b4b00caa7ae28ba687a46203bf
Richard <53105011+Richard2091@users.noreply.github.com>
2022-09-22 22:58:18 +0800

    优化代码
    更改 30分钟检查间隔为10分钟
    新增 支持自动判断新的预约
    新增 支持设置提前签到时间

--- AutoSign.js
+++ AutoSign.js
@@ -6,9 +6,9 @@
 // @grant       GM_notification
 // @connect     office.chaoxing.com
 // @connect     www.pushplus.plus
-// @version     1.0
+// @version     1.1
 // @author      Richard
-// @description 每30分钟检查签到, 并设置定时器, 到点自动签到/签退
+// @description 每10分钟检查签到, 并设置定时器, 到点自动签到/签退
 // @icon        https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/regular/calendar-check.svg
 // @require     https://cdn.jsdelivr.net/npm/dayjs@1.11.5/dayjs.min.js
 // ==/UserScript==
@@ -16,6 +16,11 @@
 (function(){
     'use strict';
 
+    //签到提前时间()
+    let advanceTime = 18;
+
+    //当前等待签到的预约id
+    let reserveId = "";
     //是否已在等待签到
     let waitSign = false;
     let waitSignback = false;
@@ -56,7 +61,7 @@
                     let nowTime = dayjs().valueOf();
                     let startTime = result.data.curReserves[0].startTime;
                     let endTime = result.data.curReserves[0].endTime;
-                    let reserveId = result.data.curReserves[0].id;
+                    let curReserveId = result.data.curReserves[0].id;
                     let roomId = result.data.curReserves[0].roomId;
                     let seatId = result.data.curReserves[0].seatId;
                     let seatNum = result.data.curReserves[0].seatNum;
@@ -64,13 +69,21 @@
                     let parameter = "?id="+reserveId+"&roomId="+roomId+"&seatId="+seatId+"&seatNum="+seatNum;
                     //console.log(URL+"sign"+parameter);
 
+                    //新的预约
+                    if(curReserveId != reserveId){
+                        //转移到新预约
+                        reserveId = curReserveId;
+                        //把旧定时器的标记关闭
+                        waitSign = false;
+                        waitSignback = false;
+                    }
                     //还未签到
                     if(startTime > nowTime && !waitSign){
-                        console.log("已获取签到时间, 等待中");
+                        console.log("已获取签到时间"+dayjs()+", 等待中");
                         //等待签到
                         waitSign = true;
                         //开启签到定时器
-                        sign(seatNum, URL+"sign"+parameter, "签到", 5000);
+                        sign(seatNum, URL+"sign"+parameter, "签到", startTime-nowTime-(advanceTime*60000));
                     }
 
                     //还未签退
@@ -92,6 +105,6 @@
     //定时检查
     let timetest = setInterval(()=>{
         checkReserve();
-    }, 1000*60*30); //每隔30分钟检查
+    }, 1000*60*10); //每隔10分钟检查
 
 })()
