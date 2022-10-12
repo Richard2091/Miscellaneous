34af45a073b6800bdca9d06e78c29b32ca2bd2f0
Richard <53105011+Richard2091@users.noreply.github.com>
2022-10-12 18:10:52 +0800

    更改 通知函数
    由 console.log 改为 GM_log，增强兼容性

--- AutoSign.js
+++ AutoSign.js
@@ -7,7 +7,7 @@
 // @grant       GM_log
 // @connect     office.chaoxing.com
 // @connect     www.pushplus.plus
-// @version     1.9.4
+// @version     1.10.0
 // @author      Richard
 // @description 每10分钟检查签到, 并设置定时器, 到点自动签到/签退，被监督自动落座
 // @icon        https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/regular/calendar-check.svg
@@ -33,7 +33,7 @@
     //本地+推送 通知服务
     function information(seatNum, type, result, msg){
         //浏览器通知
-        console.log("座位 "+seatNum+" "+type+result);
+        GM_log("座位 "+seatNum+" "+type+result);
         //如果填了token则推送
         if(token != ""){
             let title = "座位 "+seatNum+" "+type+result;
@@ -66,7 +66,7 @@
                     //操作失败
                     else{
                         information(seatNum, type, "失败", result.msg+"<br>"+URL+"<br>打开链接手动"+type);
-                        console.log("打开链接手动"+type+": "+URL);
+                        GM_log("打开链接手动"+type+": "+URL);
                     }
                 }
             });
@@ -112,13 +112,13 @@
 
                         //是否已在签到时间
                         if(nowTime>=startTime-20*60000){
-                            console.log("当前预约:"+seatNum+", 立即签到");
+                            GM_log("当前预约:"+seatNum+", 立即签到");
                             //设置立即签到
                             waitTime = 0;
                         }
                         //还没到签到时间段
                         else{
-                            console.log("当前预约:"+seatNum+", 等待签到");
+                            GM_log("当前预约:"+seatNum+", 等待签到");
                             //设置提前签到
                             waitTime = startTime-nowTime;
                         }
@@ -136,13 +136,13 @@
 
                         //已经到了后半段
                         if(nowTime > endTime){
-                            console.log("当前预约:"+seatNum+", 立即签退");
+                            GM_log("当前预约:"+seatNum+", 立即签退");
                             //立即签退
                             waitTime = 0;
                         }
                         //还没到签到正点
                         else{
-                            console.log("当前预约:"+seatNum+", 等待签退");
+                            GM_log("当前预约:"+seatNum+", 等待签退");
                             //开启签退定时器
                             waitTime = endTime-nowTime;
                         }
